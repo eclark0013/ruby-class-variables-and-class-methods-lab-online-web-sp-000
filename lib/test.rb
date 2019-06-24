b4dc863d41d85b7885e7a80f039d8f7c8acef16f
Eric Clark <eclark0013@gmail.com>
2019-06-24 12:54:11 +0000

    Automatically backed up by Learn

--- lib/test.rb
+++ lib/test.rb
@@ -1,3 +1,7 @@
-["a","b", "c", "a"].each do |letter|
-  puts ["a","b", "c", "a"].index(letter)
+for i in (0..["a","b", "c", "a"].length)
+  puts i
 end
+
+#  ["a","b", "c", "a"].each do |letter|
+#    puts ["a","b", "c", "a"].index(letter)
+#  end
